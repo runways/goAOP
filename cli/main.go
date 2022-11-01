5d1a6dd9a55dccf92da136115ef56d22fb6b0bce
andy_zhangtao <ztao8607@gmail.com>
2022-11-01 22:10:51 +0800

    #### [Optimization code]: Output more info in cli runtime

--- cli/main.go
+++ cli/main.go
@@ -8,50 +8,63 @@ import (
 )
 
 func main() {
-	
+
 	flag.Parse()
-	
+
 	check()
-	
+
 	c, err := parseConfig(*conf)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
-	
+
 	if *debug {
 		outputConfig(c)
 	}
-	
+
 	pkgs, err := aops.ParseDir(*dir, nil)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
-	
-	if *debug {
-		fmt.Println("Will handler bellow files:")
-		for key := range pkgs {
-			fmt.Println(key)
-		}
-		fmt.Println("=======>")
-	}
+
 	aopMap := make(map[string]struct{})
 	for name := range c.MiddWareMap {
 		aopMap[name] = struct{}{}
 	}
-	
+
 	pkgMap := aops.Position(pkgs, aopMap)
-	
+	if *debug {
+		fmt.Println("These files will be modify:")
+		for key := range pkgMap {
+			fmt.Println(key)
+		}
+		fmt.Println("=======>")
+	}
+
 	modify, err := aops.AddCode(pkgMap, c.MiddWareMap, *replace)
-	
+	if err != nil {
+		fmt.Println("FAILED")
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	if *debug {
+		fmt.Println("AOP result:")
+		for file, change := range pkgMap {
+			fmt.Printf("%s : %+v \n", file, change)
+		}
+		fmt.Println("=======>")
+	}
+
 	err = aops.AddImport(pkgMap, c.MiddWareMap, modify, *replace)
 	if err != nil {
 		fmt.Println("FAILED")
 		fmt.Println(err.Error())
 		os.Exit(-1)
 	}
-	
+
 	fmt.Println("// SUCCESS")
 }
 
@@ -60,7 +73,7 @@ func check() {
 		fmt.Println("dir is null, please specify source code dir path with -dir")
 		os.Exit(-1)
 	}
-	
+
 }
 
 func outputConfig(c Config) {
